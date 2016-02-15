be9b9d9f1b717663b74e8595c30be8eef8847e80
0917mabi <natem0917@yahoo.com>
2016-02-14 20:50:55 -0600

    Implement adaptive grid
    Grid size remains fixed at 500px by 500px, but now the grid will grow
    or shrink to fit inside it. Removed the 20x20 limit.

--- scripts/sketchpad.js
+++ scripts/sketchpad.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
   var gridsize;
+  var rowsize;
   $('#gridinput').on('keyup change', function() {
     gridsize = $(this).val();
-    if(gridsize > 20){
+    /*if(gridsize > 20){
       $('#gridinput').css('background-color', '#F69D9D');
       gridsize = 20;
     }else{
       $('#gridinput').css('background-color', 'white');
-    }
+    }*/
     $('#grid').empty();
     for(i=0; i<gridsize; i++) {
+      rowsize = 500/gridsize
       $('#grid').append('<div class="gridrow"></div>');
     }
     for(j=0; j<gridsize; j++) {
       $('#grid').children().append('<div class="gridcell"></div>')
     }
+    $('.gridrow').css('height', rowsize);
+    $('.gridcell').css('width', rowsize);
   });
   $(document).on('mouseenter', '.gridcell', function() {
     console.log("hola");
